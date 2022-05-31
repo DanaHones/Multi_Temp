97a258d9ea58f0f56f61368ef119c7f241d98ddb
DanaHones <danahones@gmail.com>
2022-05-31 16:10:01 -0600

    Before adding REST call

--- src/Multi_Temp.ino
+++ src/Multi_Temp.ino
@@ -5,16 +5,22 @@ static uint32_t msSample = 0;
 const int MAXRETRY = 3;
 const uint32_t msSampleTime = 10000;
 const int16_t dsData = D4;
-const int16_t dateStringSize = 22;
+const int16_t jsonBufferSize = 256;
+const int16_t dateStringSize = 15;
+const int16_t timeStringSize = 15;
 const int16_t sensorIdStringSize = 25;
+const int BadValue = -100;
 
 DS18B20 ds18b20(dsData);
 
 const int nSENSORS = 2;
-float celsius[nSENSORS] = {NAN, NAN};
-retained uint8_t sensorAddresses[nSENSORS][8];
+//retained uint8_t sensorAddresses[nSENSORS][8];
+uint8_t sensorAddresses[nSENSORS][8];
+String sensorHexString[nSENSORS];
 char dateString[dateStringSize] = {0};
+char timeString[timeStringSize] = {0};
 char sensorIdString[sensorIdStringSize] = {0};
+char json_buffer[jsonBufferSize];
 String Running = "RUN";
 
 void setup()
@@ -22,14 +28,32 @@ void setup()
   Particle.variable("Running", Running);
 	Particle.function("SetRunning", SetRunning);
 
-  ds18b20.resetsearch();                 // initialise for sensor search
+  // Time zone for MST
+  Time.zone(-6);
 
-  for (int i = 0; i < nSENSORS; i++)
-  {  // try to read the sensor addresses
-    ds18b20.search(sensorAddresses[i]);  // and if available store
+  if (2 == 1)
+  {
+    // Look for new sensors
+    ds18b20.resetsearch();                 // initialize for sensor search
+    for (int i = 0; i < nSENSORS; i++)
+    {  // try to read the sensor addresses
+      ds18b20.search(sensorAddresses[i]);  // and if available store
+    }
+  }
+  else
+  {
+    // Sensor 1
+    sensorHexString[0] = "28:DE:29:95:F0:01:3C:F6";
+    sensorAddresses[0][0] = 0x28; sensorAddresses[0][1] = 0xDE; sensorAddresses[0][2] = 0x29; sensorAddresses[0][3] = 0x95; 
+    sensorAddresses[0][4] = 0xF0; sensorAddresses[0][5] = 0x01; sensorAddresses[0][6] = 0x3C; sensorAddresses[0][7] = 0xF6; 
+
+    // Sensor 2
+    sensorHexString[1] = "28:5F:9F:95:F0:01:3C:F8";
+    sensorAddresses[1][0] = 0x28; sensorAddresses[1][1] = 0x5F; sensorAddresses[1][2] = 0x9F; sensorAddresses[1][3] = 0x95; 
+    sensorAddresses[1][4] = 0xF0; sensorAddresses[1][5] = 0x01; sensorAddresses[1][6] = 0x3C; sensorAddresses[1][7] = 0xF8; 
   }
 
-  Serial.println("Let's get started");
+  Serial.println("Let's get started!");
 }
 
 void loop()
@@ -38,14 +62,19 @@ void loop()
   {
     if (millis() - msSample >= msSampleTime)
     {
+      Set_DateTime();
       msSample = millis();
-      Show_DateTime(true);
       for (int i = 0; i < nSENSORS; i++)
       {
         float temp = getTemp(sensorAddresses[i]);
-        if (!isnan(temp)) celsius[i] = temp;
+        if (isnan(temp))
+        {
+          temp = BadValue;
+          Serial.println("Wooo");
+        }
+        Create_TempZone_Json(json_buffer, jsonBufferSize, dateString, timeString, sensorHexString[i], temp);
+        Serial.println(json_buffer);
       }
-      Serial.println("");
     }
   }
 }
@@ -63,13 +92,13 @@ double getTemp(uint8_t addr[8]) {
   {
     _temp = ds18b20.convertToFahrenheit(_temp);
     //Serial.print(addr);
-    showAddress(addr, false);
-    Serial.print(" - ");
-    Serial.println(_temp);
+    //showAddress(addr, false);
+    //Serial.print(" - ");
+    //Serial.println(_temp);
   }
   else
   {
-    _temp = NAN;
+    _temp = BadValue;
     Serial.println("Invalid reading");
   }
 
@@ -91,27 +120,22 @@ void showAddress(uint8_t addr[8], bool lf)
   }
 }
 
-void Show_DateTime(bool lf) 
+void Set_DateTime() 
 {
     dateString[dateStringSize] = {0};
+    timeString[timeStringSize] = {0};
     
-    sprintf(dateString, "%02u/%02u/%04u - %02u:%02u:%02u:",
+    sprintf(dateString, "%02u/%02u/%04u",
       Time.month(),
       Time.day(),
-      Time.year(),
+      Time.year()
+      );
+
+    sprintf(timeString, "%02u:%02u:%02u",
       Time.hour(),
       Time.minute(),
       Time.second()
       );
-    
-    if (lf)
-    {
-      Serial.println(dateString);
-    }
-    else
-    {
-      Serial.print(dateString);
-    }
 }
 
 int SetRunning (String value)
@@ -119,6 +143,26 @@ int SetRunning (String value)
  
     Running = value;
     return 1;
+}
+
+void Create_TempZone_Json(char *ptr, int size, String date, String time, String sensorId, int zoneTemp)
+{
+    //memset(ptr, 0, sizeof(ptr));
+    
+    //Serial.println(sizeof(json_buffer));
+    //Serial.println(sizeof(*ptr));
+    
+    JSONBufferWriter writer(ptr, size);
+    //JSONBufferWriter writer(json_buffer, sizeof(json_buffer));
+    
+    writer.beginObject();
+        writer.name("date").value(date);
+        writer.name("time").value(time);
+        writer.name("sensorid").value(sensorId);
+        writer.name("zonetemp").value(zoneTemp);
+    writer.endObject();
+    //writer.buffer()[std::min(writer.bufferSize(), writer.dataSize())] = 0;
+    //Serial.println(json_buffer);
 }
 
 /*
